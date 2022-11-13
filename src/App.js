360944c8a2b8beeaa1215b968b41d901c6fdf8c6
CassioAA <cassioalmeidaccti@gmail.com>
2022-11-13 18:39:40 -0300

    Cards

--- src/App.js
+++ src/App.js
@@ -28,19 +28,58 @@ function App() {
 
   return (
     <>
-      <form>
-          <input id='id' type="text" placeholder='id' />
-          <input id='description' type="text" placeholder='description' />
-          <button onClick={addTask}>+ Task</button>
+      <form className="row g-3">
+          <div className="col-md-6">
+              <label for="inputEmail4" className="form-label">Id</label>
+              <input id='id' type="text" className="form-control" />
+          </div>
+          <div className="col-md-6">
+              <label for="inputEmail4" className="form-label">Description</label>
+              <input id='description' type="text" className="form-control" />
+          </div>
+          <br/>
+          <div className='col-12'>
+            <button 
+              className="bnt btn-outline-secondary" 
+              onClick={addTask}
+            >
+              + Task
+            </button>
+          </div>
       </form>
       <div className="mt-3">
-        <ul className='list-group'>
           {tasks.map(t => (
-            <li key={t.id} className='list-group-item'>
-                {t.id} - {t.description}
-            </li>
+              <div key={t.id} className="card mb-2 shadow-sm">
+                <div className="card-body">
+                      <div className="d-flex justify-content-between">
+                        <h5 className="card-title">
+                          <span className="badge bg-secondary me-1">
+                            {t.id}
+                          </span>
+                          - title
+                        </h5>
+                        <h6>
+                            Priority: 
+                            <span className="ms-1 text-black">
+                              <i className="me-1 far fa-meh"></i>
+                              Normal
+                            </span>
+                        </h6>
+                      </div>
+                    <p className="card-text">{t.description}</p>
+                    <div className="d-flex justify-content-end pt-2 m-0 border-top">
+                      <button className='btn btn-sm btn-outline-primary me-2'>
+                        <i className='fas fa-pen me-2'></i>
+                        Edit
+                      </button>
+                      <button className='btn btn-sm btn-outline-danger'>
+                      <i className='fas fa-trash me-2'></i>
+                        Delete
+                      </button>
+                    </div>
+                </div>
+              </div>
           ))}
-        </ul>
       </div>
     </>
   );
