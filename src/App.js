036e35ddcc99c158444391d84ce40fe596370107
CassioAA <cassioalmeidaccti@gmail.com>
2022-11-14 19:09:40 -0300

    priority, deleteTask

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 let initialState = [
   {
     id : 1,
+    priority : '1',
+    title : "title",
     description : "First Task"
   },
   {
     id : 2,
+    priority : '3',
+    title : "title",
     description : "Second Task"
   }
 ]
@@ -20,22 +24,95 @@ function App() {
     e.preventDefault();
     const task = {
       id: document.getElementById('id').value,
+      priority: document.getElementById('priority').value,
+      title: document.getElementById('title').value,
       description: document.getElementById('description').value,
     };
-    console.log(tasks);
+
     setTasks( [ ...tasks, {...task}] );
+
+  }
+
+  function deleteTask(id) {
+    // e.preventDefault();
+    const filteredTasks = tasks.filter(t => t.id !== id)
+
+    setTasks( [ ...filteredTasks] );
+
+  }
+
+  function priorityLabel(param) {
+    switch(param) {
+      case '1':
+        return 'Low';
+      case '2':
+        return 'Normal';
+      case '3':
+        return 'High';
+      default:
+        return 'Unkown';
     }
+  }
+
+  function priorityStyle(param, icon) {
+    switch(param) {
+      case '1':
+        return icon ? 'smile' : 'success';
+      case '2':
+        return icon ? 'meh' : 'dark';
+      case '3':
+        return icon ? 'frown' : 'warning';
+      default:
+        return 'Unkown';
+    }
+  }
 
   return (
     <>
       <form className="row g-3">
           <div className="col-md-6">
-              <label for="inputEmail4" className="form-label">Id</label>
-              <input id='id' type="text" className="form-control" />
+              <label className="form-label">Id</label>
+              <input 
+              id='id' 
+              type="text" 
+              className="form-control" 
+              readOnly
+              value={
+                Math.max.apply(
+                  Math, 
+                  tasks.map(t => t.id)
+                ) + 1
+              }
+          />
           </div>
           <div className="col-md-6">
-              <label for="inputEmail4" className="form-label">Description</label>
-              <input id='description' type="text" className="form-control" />
+            <label className="form-label">Priority</label>
+            <select id="priority" className="form-select">
+              <option defaultValue="0">Choose...</option>
+              <option value="1">Low</option>
+              <option value="2">Normal</option>
+              <option value="3">High</option>
+            </select>
+          </div>
+          <div className="col-md-6">
+              <label className="form-label">
+                  Title
+              </label>
+              <input 
+                  id='title' 
+                  type="text" 
+                  className="form-control" 
+              />
+          </div>
+          <div className="col-md-6">
+              <label className="form-label">
+                  Description
+              </label>
+              <input 
+                  id='description' 
+                  type="text" 
+                  className="form-control" 
+              />
           </div>
           <br/>
           <div className='col-12'>
@@ -49,20 +126,20 @@ function App() {
       </form>
       <div className="mt-3">
           {tasks.map(t => (
-              <div key={t.id} className="card mb-2 shadow-sm">
+              <div key={t.id} className={"card mb-2 shadow-sm border-" + priorityStyle(t.priority)}>
                 <div className="card-body">
                       <div className="d-flex justify-content-between">
                         <h5 className="card-title">
                           <span className="badge bg-secondary me-1">
                             {t.id}
                           </span>
-                          - title
+                          - {t.title}
                         </h5>
                         <h6>
                             Priority: 
-                            <span className="ms-1 text-black">
-                              <i className="me-1 far fa-meh"></i>
-                              Normal
+                            <span className={"ms-1 text-" + priorityStyle(t.priority)}>
+                              <i className={"me-1 far fa-" + priorityStyle(t.priority, true)}></i>
+                              {priorityLabel(t.priority)}
                             </span>
                         </h6>
                       </div>
@@ -72,7 +149,9 @@ function App() {
                         <i className='fas fa-pen me-2'></i>
                         Edit
                       </button>
-                      <button className='btn btn-sm btn-outline-danger'>
+                      <button 
+                        className='btn btn-sm btn-outline-danger'
+                        onClick={() => deleteTask(t.id)}>
                       <i className='fas fa-trash me-2'></i>
                         Delete
                       </button>
